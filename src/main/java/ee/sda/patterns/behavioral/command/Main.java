1a71407f794f61580013a57d0bd3e235ad115803
Dmitri Maksimov <dmitri.maksimov@nortal.com>
2018-09-21 19:50:23 +0300

    Implemented Command.

--- src/main/java/ee/sda/patterns/behavioral/command/Main.java
+++ src/main/java/ee/sda/patterns/behavioral/command/Main.java
@@ -1,8 +1,27 @@
 package ee.sda.patterns.behavioral.command;
 
+import ee.sda.patterns.behavioral.command.model.Computer;
+
 public class Main {
 
   public static void main(String[] args) {
+    Computer computer = new Computer();
+
+    PowerOnComputerCommand powerOnComputerCommand = new PowerOnComputerCommand(computer);
+    CalculationComputerCommand calculationComputerCommand
+        = new CalculationComputerCommand(computer, "1+1");
+    PowerOffComputerCommand powerOffComputerCommand = new PowerOffComputerCommand(computer);
+
+    System.out.println(computer.getState());
+
+    powerOnComputerCommand.execute();
+    System.out.println(computer.getState());
+
+    calculationComputerCommand.execute();
+    System.out.println(computer.getState());
+
+    powerOffComputerCommand.execute();
+    System.out.println(computer.getState());
 
   }
 
